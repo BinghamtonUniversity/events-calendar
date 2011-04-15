cfc5256f6d3638ca1872b673a747c133c8d2f8aa
Patrick Lewis <plewis@binghamton.edu>
2011-04-15 11:28:22 -0400

    Initial commit

---
+++ application/logs/2011/03/14.php
@@ -0,0 +1,5 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2011-03-14 06:25:08 --- ERROR: Kohana_Exception [ 0 ]: hello, world! ~ SYSPATH/tests/kohana/ExceptionTest.php [ 50 ]
+2011-03-14 06:25:08 --- ERROR: ErrorException [ 0 ]: hello, world! ~ SYSPATH/tests/kohana/ExceptionTest.php [ 50 ]
+2011-03-14 06:25:08 --- ERROR: Kohana_Exception [ 0 ]:  ~ SYSPATH/tests/kohana/ExceptionTest.php [ 50 ]
