0bb61c9dc81302833993036c4a23e1744ca65c22
Patrick Lewis <plewis@binghamton.edu>
2011-04-22 16:47:37 -0400

    Make some progress with styling

--- application/views/pages/event.php
+++ application/views/pages/event.php
@@ -7,11 +7,13 @@
 
     echo sprintf('<div class="event" data-calendar="%s">', $event->calendar->title);
     echo '<p class="event_header">';
+    echo '<span class="event_title">';
     echo sprintf('<a class="event_link" data-event="%s" href="%s">%s</a>',
         $event->permalink,
         URL::site('calendar/event/'.$event->permalink),
         $event->title
-    ).'<br/>';
+    );
+    echo '</span><br/>';
     echo $event->human_date.' '.$start_time;
     echo '</p>';
     if ($event->content) {
