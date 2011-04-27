efffe586c2dc6be0b67cc447fb1183f7bc5438f9
Patrick Lewis <plewis@binghamton.edu>
2011-04-27 12:00:44 -0400

    Add JSON format for feeds

--- application/classes/controller/feed.php
+++ application/classes/controller/feed.php
@@ -2,6 +2,19 @@
 
 class Controller_Feed extends Controller {
 
+    private function _prepare_event($feed_event)
+    {
+        $event = new stdClass;
+        $event->title      = $feed_event->title;
+        $event->date       = $feed_event->date;
+        $event->start_time = $feed_event->start_time;
+        $event->end_time   = $feed_event->end_time;
+        $event->permalink  = $feed_event->permalink;
+        $event->content    = $feed_event->content;
+
+        return $event;
+    }
+
     public function action_index()
     {
         $feeds = ORM::factory('feed')
@@ -59,6 +72,7 @@ class Controller_Feed extends Controller {
         $view = View::factory('template')
             ->bind('feed', $feed)
             ->bind('feed_events', $feed_events)
+            ->bind('feed_events_json', $feed_events_json)
             ->bind('available_events', $events);
 
         $view->subview = 'pages/edit_feed';
@@ -78,5 +92,19 @@ class Controller_Feed extends Controller {
         $feed = ORM::factory('feed', $feed_id);
         $feed->remove('events', $event_permalink);
         $this->request->redirect('feed/edit/'.$feed_id);
+    }
+
+    // Output a JSON-encoded version of the specified feed
+    public function action_json($feed_id)
+    {
+        $feed             = ORM::factory('feed', $feed_id);
+        $feed_events      = $feed->events->order_by('date')->find_all()->as_array();
+        $feed_events_json = array();
+
+        foreach ($feed_events as $event) {
+            array_push($feed_events_json, $this->_prepare_event($event));
+        }
+
+        echo json_encode($feed_events_json);
     }
 }
