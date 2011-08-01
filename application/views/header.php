2fa80cd9fbd5d67b1639a6c76ce5d1b221c006d1
Patrick Lewis <plewis@binghamton.edu>
2011-08-01 13:59:16 -0400

    Update paths

--- application/views/header.php
+++ application/views/header.php
@@ -7,7 +7,7 @@
         <img class="logoLink" src="http://www.binghamton.edu/images/siteLogos/logo-events.gif" alt="Binghamton University, State University of New York - Events Calendar" usemap="#Map" />
         <map name="Map" id="Map">
             <area shape="rect" coords="0,0,210,66" href="/index.html" alt="" />
-            <area shape="rect" coords="220,0,407,66" href="/events/" alt="" />
+            <area shape="rect" coords="220,0,407,66" href="/" alt="" />
         </map>
 			
 <form action="http://www.binghamton.edu/results.html" id="cse-search-box">
