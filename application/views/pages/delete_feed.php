4edaf5b1983e23eaabe1c7d5534bdf4f18aa13da
Patrick Lewis <plewis@binghamton.edu>
2012-01-31 11:11:14 -0500

    Clean up text in delete_feed.php

--- application/views/pages/delete_feed.php
+++ application/views/pages/delete_feed.php
@@ -1,9 +1,9 @@
 <div style="margin-top: 20px;">
 <?php
-    echo sprintf('Are you sure you want to delete the <strong>%s</strong> feed?',
+    echo sprintf('Are you sure you want to delete the <strong>%s</strong> feed? ',
         $feed->title
     );
-    echo sprintf('<a href="%s">Yes</a>',
+    echo sprintf('<a href="%s">Yes</a> ',
         URL::site('feed/confirm_delete/'.$feed->id)
     );
     echo sprintf('<a href="%s">No</a>',
