fd1c22cefa91c2441696849a05c5f824668d0995
Patrick Lewis <plewis@binghamton.edu>
2012-11-05 13:03:48 -0500

    Add IDs and classes to homepage include

--- application/views/homepage-include.php
+++ application/views/homepage-include.php
@@ -1,12 +1,12 @@
-<h1><span class="grey">UNIVERSITY</span> CALENDAR<!-- <img class="rssIcon" src="http://www2.binghamton.edu/inside/images/rss-logo.png" _mce_src="http://www2.binghamton.edu/inside/images/rss-logo.png" alt="RSS feed logo" width="16" height="16" /> --><strong></strong><strong></strong></h1>
-<ul>
+<h1 id="events-calendar-heading"><span class="grey">UNIVERSITY</span> CALENDAR</h1>
+<ul id="events-calendar-list">
 
 <?php
     foreach ($feed_events as $event) {
         $formatted_date = strftime('%b %e', strtotime($event->date));
-        echo "<li><strong>$formatted_date:</strong> <a href=\"http://calendar.binghamton.edu/index.php/calendar/event/$event->permalink\">$event->title</a></li>";
+        echo "<li class=\"events-calendar-event\"><strong>$formatted_date:</strong> <a href=\"http://calendar.binghamton.edu/index.php/calendar/event/$event->permalink\">$event->title</a></li>";
     }
 ?>
 
 </ul>
-<p><a href="http://calendar.binghamton.edu/">More University Events →</a></p>
+<p id="events-calendar-link"><a href="http://calendar.binghamton.edu/">More University Events →</a></p>
