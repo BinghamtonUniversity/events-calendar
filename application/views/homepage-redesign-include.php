1c16adea9d31546814cda78e823a9077fd46825e
Patrick Lewis <plewis@binghamton.edu>
2012-12-21 10:09:02 -0500

    Replace homepage feed include's inline styles with CSS ids/classes

--- application/views/homepage-redesign-include.php
+++ application/views/homepage-redesign-include.php
@@ -1,10 +1,10 @@
-<div class="cubes" style="margin-left: 0; margin-bottom: 15px">
+<div class="cubes" id="cubeCalendar">
   <div class="greenHeader">
     <h4 class="spaced">
-      <span class="h4Topics" style="color: #ccc">Featured</span> Events<a href="http://calendar.binghamton.edu/"><img src="http://www.binghamton.edu/gerald/redesign13/images/calendar-white.png" alt="calendar" style="display: inline; padding: 5px 5px 0 0; float: right" /></a>
+      <span class="h4Topics h4Featured">Featured</span> Events<a href="http://calendar.binghamton.edu/"><img src="http://www.binghamton.edu/gerald/redesign13/images/calendar-white.png" alt="calendar" /></a>
     </h4>
   </div>
-  <ul style="margin: 0; padding: 5px 0 0 10px">
+  <ul>
     <?php
       foreach ($feed_events as $event) {
         $formatted_date = strftime('%B %e', strtotime($event->date));
@@ -15,7 +15,7 @@
           $day_of_week = strftime('%A', strtotime($event->date));
         }
     ?>
-      <li style="list-style-type: none; text-transform: uppercase; font-size: 13px; margin: 0 0 5px 0"><strong><?php echo $day_of_week; ?>, <span style="font-weight: 100;"><?php echo $formatted_date; ?></span></strong><br /> <a href="<?php echo $link; ?>" style="text-transform: none; font-size: 18px"><?php echo $event->title; ?></a></li>
+      <li><strong><?php echo $day_of_week; ?>,</strong> <span class="calendarDates"><?php echo $formatted_date; ?></span><br /> <a href="<?php echo $link; ?>"><?php echo $event->title; ?></a></li>
     <?php
       }
     ?>
