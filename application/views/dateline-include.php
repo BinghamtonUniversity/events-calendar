90b94de83932992b1cee401b9b6b45dc94fce27f
Patrick Lewis <plewis@binghamton.edu>
2013-01-18 14:35:46 -0500

    Update inline styles for Dateline include

--- application/views/dateline-include.php
+++ application/views/dateline-include.php
@@ -1,10 +1,10 @@
 <h1 id="events-calendar-heading" style="color: #00694d; font-size: 14px; padding-left: 0px; padding-top: 10px; text-transform: none; font-family: Arial; font-weight: normal;" >UNIVERSITY CALENDAR</h1>
-<ul id="events-calendar-list" style="left-index: 0px; font-family: Arial; font-weight: normal;padding-left: 20px; list-style-type: none;" >
+<ul id="events-calendar-list" style="left-index: 0px; font-family: Arial; font-weight: normal; list-style-type: none;" >
 
 <?php
     foreach ($feed_events as $event) {
         $formatted_date = strftime('%b %e', strtotime($event->date));
-        echo "<li class=\"events-calendar-event\" style=\"color: #999999; font-size: 12px; padding-right: 10px; margin-left: -10px; font-family: Arial; font-weight: normal;\" ><strong>$formatted_date:</strong> <a style=\"color: #00694d; font-size: 12px; font-family: Arial; font-weight: normal;\" href=\"http://calendar.binghamton.edu/index.php/calendar/event/$event->permalink\">$event->title</a></li>";
+        echo "<li class=\"events-calendar-event\" style=\"color: #999999; font-size: 12px; padding-right: 10px; font-family: Arial; font-weight: normal;\" ><strong>$formatted_date:</strong> <a style=\"color: #00694d; font-size: 12px; font-family: Arial; font-weight: normal;\" href=\"http://calendar.binghamton.edu/index.php/calendar/event/$event->permalink\">$event->title</a></li>";
     }
 ?>
 
