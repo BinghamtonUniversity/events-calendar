0023c6defb32d3fdb7f10b50927be378e9dff1af
Patrick Lewis <plewis@binghamton.edu>
2013-03-11 09:38:01 -0400

    Add current University footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,20 +1,31 @@
 <!-- begin footer -->
 <div class="contentWidth footerOuter">
-    <div class="footerLeft">
-        <div class="addlInfo" style="padding-top: 16px">
-            <a href="http://twitter.com/binghamtonu"><img style="display: inline;" src="http://www.binghamton.edu/images/twitter-icon.png" width="32" height="32" alt="Twitter icon links to Binghamton University's Twitter page"/></a>
-            <a href="http://www.youtube.com/user/BinghamtonUniversity"><img style="display: inline; padding-left: 8px" src="http://www.binghamton.edu/images/youtube-icon.png" alt="YouTube icon links to Binghamton University's YouTube page" width="32" height="32"/></a>
-            <a href="http://www.facebook.com/pages/Binghamton-NY/Binghamton-University/51791915551"><img style="display: inline; padding-left: 8px" src="http://www.binghamton.edu/images/facebook-icon.png" alt="Facebook icon links to Binghamton University's Facebook page" width="32" height="32" /></a>
-            <div class="copyright">
-                <a href="http://www.binghamton.edu/magazine/">Binghamton Magazine</a> | <a href="http://www.binghamton.edu/inside/">Inside Binghamton</a> | <a href="http://www.binghamton.edu/photos">Daily Photo</a><br/>
-                &copy; 2011 Binghamton University, State University of New York
-            </div>
-        </div>
+    <div class="footerLeft" style="width: 340px; padding-left: 20px;">
+    <div class="addlInfo">
+    <div style="margin-bottom: 5px"><em>Connect with Binghamton:</em></div>
+    <a href="http://twitter.com/binghamtonu">
+            <img style="display: inline;" src="http://www.binghamton.edu/images/twitter-icon.png" alt="Twitter icon links to Binghamton University's Twitter page"/>
+          </a>
+
+          <a href="http://www.youtube.com/user/BinghamtonUniversity">
+              <img style="display: inline; padding-left: 8px" src="http://www.binghamton.edu/images/youtube-icon.png" alt="YouTube icon links to Binghamton University's YouTube page"/>
+          </a>
+          <a href="http://www.facebook.com/pages/Binghamton-NY/Binghamton-University/51791915551">
+              <img style="display: inline; padding-left: 8px" src="http://www.binghamton.edu/images/facebook-icon.png" alt="Facebook icon links to Binghamton University's Facebook page"/>
+          </a>
+          <a href="http://pinterest.com/binghamtonu/"><img style="display: inline; padding-left: 9px" src="http://www.binghamton.edu/images/pinterest.png" alt="Pinterest icon links to Binghamton University's Pinterest page" width="32" height="32" /></a>
+
     </div>
-    <div class="footerRight" style="width: 161px; padding-right: 10px;">
-        <div class="feedsNetworks">
-            <a href="http://www.binghamton.edu/giving/"><img src="http://www.binghamton.edu/images/home/bold-brilliant-binghamton.png" width="161" height="81" alt="Bold, Brilliant, Binghamton - The Campaign for Binghamton University" /></a>
-        </div>
+  </div>
+    <div class="footerRight" style="width: 570px; padding-right: 20px; line-height: 180%;">
+    <div class="copyright" style="text-align: right">&copy; <?php echo date('Y'); ?> Binghamton University, State University of New York<br />
+        <a href="http://www2.binghamton.edu/magazine/">Binghamton Magazine</a> | <a href="http://www2.binghamton.edu/inside/">Inside Binghamton</a> | <a href="http://www.binghamton.edu/photos">Daily Photo</a><br />
+        <a href="http://www.binghamton.edu/about/contact-us.html">Contact Us</a> | <a href="http://www.telecom.binghamton.edu/directory/directory.search">Directory</a></div>
+    <div class="feedsNetworks">
+
+    <div class="shareLinks">
+
+      </div>
     </div>
+  </div>
 </div>
-<!-- end footer -->
